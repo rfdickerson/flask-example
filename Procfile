b6f69f4f18acc31e76af0c7ec9f018d6920da460
Robert F. Dickerson <rfdickerson@us.ibm.com>
2017-06-07 08:25:53 -0500

    cloud foundry support and gunicorn

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn -b :`echo $PORT` server:app 
--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,11 @@
+"""basic Flask app entry"""
+
+import os
 
 from flask import Flask
+
+PORT = int(os.getenv('PORT', 5000))
+
 app = Flask(__name__)
 
 from server import routes
